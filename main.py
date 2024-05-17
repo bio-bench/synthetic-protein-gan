78dd6b97501143f8d46b4d29c03ffc0ad79d3573
Saleem <saleem.m@gmail.com>
2024-05-17 12:03:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from utils.data_processing import read_fasta_and_weights, one_hot_encode_sequence, decode_one_hot_sequence
 from utils.models import build_generator, build_discriminator
-from utils.training import train_gan, generate_sequences
+from utils.training import train_gan, generate_sequences, SequenceDataGenerator
 import numpy as np
+import tensorflow as tf
+
+# Ensure TensorFlow uses GPU
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 # Define file path and parameters
 file_path = 'data/your_sequences.fasta'  # Path to your plain FASTA file
